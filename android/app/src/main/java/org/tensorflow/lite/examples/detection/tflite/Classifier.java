ebe0d0c04a5db722f8bd351eeb4ff8fb25c83305
unknown <oriothechoker@gmail.com>
2022-07-14 00:53:10 +0700

    new

---
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/tflite/Classifier.java
@@ -0,0 +1,133 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+package org.tensorflow.lite.examples.detection.tflite;
+
+import android.graphics.Bitmap;
+import android.graphics.RectF;
+
+import java.util.List;
+
+/**
+ * interface chung cho việc tương tác giữa các engine khác nhau
+ */
+public interface Classifier {
+    List<Recognition> recognizeImage(Bitmap bitmap);
+
+    void enableStatLogging(final boolean debug);
+
+    String getStatString();
+
+    void close();
+
+    void setNumThreads(int num_threads);
+
+    void setUseNNAPI(boolean isChecked);
+
+    abstract float getObjThresh();
+
+    /**
+     * class trả về thông tin vật được detect
+     */
+    public class Recognition {
+        /**
+         định danh vật được detect
+         */
+        private final String id;
+
+        /**
+         * tên của vật được detect
+         */
+        private final String title;
+
+        /**
+         * độ tin cậy (càng cao càng tốt)
+         */
+        private final Float confidence;
+
+        /**
+         * địa điểm của vật được detect
+         */
+        private RectF location;
+
+        private int detectedClass;
+
+        public Recognition(
+                final String id, final String title, final Float confidence, final RectF location) {
+            this.id = id;
+            this.title = title;
+            this.confidence = confidence;
+            this.location = location;
+        }
+
+        public Recognition(final String id, final String title, final Float confidence, final RectF location, int detectedClass) {
+            this.id = id;
+            this.title = title;
+            this.confidence = confidence;
+            this.location = location;
+            this.detectedClass = detectedClass;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Float getConfidence() {
+            return confidence;
+        }
+
+        public RectF getLocation() {
+            return new RectF(location);
+        }
+
+        public void setLocation(RectF location) {
+            this.location = location;
+        }
+
+        public int getDetectedClass() {
+            return detectedClass;
+        }
+
+        public void setDetectedClass(int detectedClass) {
+            this.detectedClass = detectedClass;
+        }
+
+        @Override
+        public String toString() {
+            String resultString = "";
+            if (id != null) {
+                resultString += "[" + id + "] ";
+            }
+
+            if (title != null) {
+                resultString += title + " ";
+            }
+
+            if (confidence != null) {
+                resultString += String.format("(%.1f%%) ", confidence * 100.0f);
+            }
+
+            if (location != null) {
+                resultString += location + " ";
+            }
+
+            return resultString.trim();
+        }
+    }
+}
